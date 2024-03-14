c40f607dd0bb3d2fca31fb6c6e648002b4fab230
PresentKim <debe3721@gmail.com>
2024-03-14 16:30:46 +0900

    :sparkles: Add registerFromDirectory() method

--- src/kim/present/register/resourcepack/ResourcePackRegister.php
+++ src/kim/present/register/resourcepack/ResourcePackRegister.php
@@ -38,6 +38,7 @@ use Symfony\Component\Filesystem\Path;
 use function copy;
 use function file_get_contents;
 use function json_encode;
+use function md5;
 use function str_ends_with;
 use function str_replace;
 use function str_starts_with;
@@ -61,22 +62,36 @@ final class ResourcePackRegister{
 		)(Server::getInstance()->getResourcePackManager());
 	}
 
+
 	/**
 	 * Register resource pack from the specified directory of plugin resources
 	 *
-	 * @param bool $temp Whether archive to temporary dir. if false, archive to resource_packs dir
+	 * @param bool $isTemp Whether archive to temporary dir. if false, archive to resource_packs dir
 	 */
-	public static function registerFromResource(Plugin $plugin, string $resourcePath, bool $temp = true) : void{
-		$addonDir = $plugin->getResourcePath($resourcePath);
+	public static function registerFromResource(Plugin $plugin, string $resourcePath, bool $isTemp = true) : void{
+		self::registerFromDirectory(
+			$plugin->getResourcePath($resourcePath),
+			$isTemp,
+			$plugin->getName() . "." . str_replace("/", ".", $resourcePath)
+		);
+	}
 
+
+	/**
+	 * Register resource pack from the specified directory
+	 *
+	 * @param bool   $isTemp     Whether archive to temporary dir. if false, archive to resource_packs dir
+	 * @param string $outputName Using when $isTemp is false, if empty, use md5 hash of $addonDir
+	 */
+	public static function registerFromDirectory(string $addonDir, bool $isTemp = true, string $outputName = "") : void{
 		$tmp = tempnam(sys_get_temp_dir(), "pm$");
 		self::archiveDirectory($addonDir, $tmp);
-		if($temp){
+		if($isTemp){
 			$pack = new TempResourcePack($tmp);
 		}else{
 			$output = Path::join(
 				Server::getInstance()->getResourcePackManager()->getPath(),
-				"_resourcepack-register." . $plugin->getName() . "." . str_replace("/", ".", $resourcePath) . ".zip"
+				"_resourcepack-register." . ($outputName ?: md5($addonDir)) . ".zip"
 			);
 			copy($tmp, $output);
 			$pack = new ZippedResourcePack($output);
